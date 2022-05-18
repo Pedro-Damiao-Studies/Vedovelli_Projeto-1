6fb934263bf612a967ce725f223df4604e7103ba
Pedro Henrique Ferreira <pedrohfcd@hotmail.com>
2022-05-18 10:12:00 -0300

    Jest watch mode: observar alterações nos arquivos

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,1 +1,10 @@
-module.exports.sum = (num1, num2) => num1 + num2;
+module.exports.sum = (num1, num2) => {
+  const parsedNum1 = parseInt(num1, 10);
+  const parsedNum2 = parseInt(num2, 10);
+
+  if (isNaN(parsedNum1) || isNaN(parsedNum2)) {
+    throw new Error('Please check your input');
+  }
+
+  return parsedNum1 + parsedNum2;
+};
