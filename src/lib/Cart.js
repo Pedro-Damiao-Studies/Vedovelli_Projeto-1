2015d55e9dac5dc37a26314b14ed67a84bc64054
Pedro Henrique Ferreira <pedrohenriquefcd@gmail.com>
2022-05-21 15:12:55 -0300

    Shopping Cart: Criacao Cart.js e Cart.spec.js, primeiros testes e .editorconfig

---
+++ src/lib/Cart.js
@@ -0,0 +1,15 @@
+export default class Cart {
+
+  items = [];
+
+
+  add(item) {
+    this.items.push(item);
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, item) => {
+      return acc + (item.product.price * item.quantity);
+    }, 0);
+  }
+}
